da4b8dad77b51b78a7366380cf1482cb45fa73c8
Rogerio Pereira <hi@rogeriopereira.dev>
2026-01-05 12:55:44 -0500

    Function to normalize license plates

--- app/main.py
+++ app/main.py
@@ -13,3 +13,11 @@
 
 # total = sum(5, 10)  # Call the function that returns a value
 # print(f"The total sum is: {total}")
+
+def normalize_plate(plate):
+    # Normalize a vehicle plate by removing spaces and converting to uppercase.
+    return plate.replace(" ", "").upper()   # I can't break lines here
+
+plate = input("License Plate? ")
+normalized_plate = normalize_plate(plate)
+print(f"Normalized Plate: {normalized_plate}")
