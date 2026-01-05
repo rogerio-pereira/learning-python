666e8d664edeb302acaa69ec19f20755b4f46b40
Rogerio Pereira <hi@rogeriopereira.dev>
2026-01-05 13:28:46 -0500

    Create license plate module

--- app/main.py
+++ app/main.py
@@ -1,22 +1,4 @@
-# def say_hello(name):
-#     print(f"Calling a function, your name is {name}!")
-
-# def sum(a, b):
-#     return a + b
-
-# name = "Rogerio"
-# active = True
-
-# print(f"Hello, {name}! Active status is {active}.");    #f before " formats the string with variables
-# say_hello(name) # Call the function
-
-
-# total = sum(5, 10)  # Call the function that returns a value
-# print(f"The total sum is: {total}")
-
-def normalize_plate(plate):
-    # Normalize a vehicle plate by removing spaces and converting to uppercase.
-    return plate.replace(" ", "").upper()   # I can't break lines here
+from util.license_plate import normalize_plate
 
 plate = input("License Plate? ")
 normalized_plate = normalize_plate(plate)
---
+++ app/util/__init__.py
@@ -0,0 +1,2 @@
+# This file is intentionally left blank to mark the directory as a package.
+# It is required for Python to recognize 'util' as a module.
