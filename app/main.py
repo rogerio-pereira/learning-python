30b080856e481ff63461fed75b8a2b861086c979
Rogerio Pereira <hi@rogeriopereira.dev>
2026-01-05 12:46:57 -0500

    Calling functions

--- app/main.py
+++ app/main.py
@@ -1,4 +1,15 @@
+def sayHello(name):
+    print(f"Calling a function, your name is {name}!")
+
+def sum(a, b):
+    return a + b
+
 name = "Rogerio"
 active = True
 
 print(f"Hello, {name}! Active status is {active}.");    #f before " formats the string with variables
+sayHello(name) # Call the function
+
+
+total = sum(5, 10)  # Call the function that returns a value
+print(f"The total sum is: {total}")
