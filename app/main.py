844af2e848ceb8e8daf81c1352c3225e2ec12ec1
Rogerio Pereira <hi@rogeriopereira.dev>
2026-01-05 12:48:32 -0500

    Using naming conventions

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
-def sayHello(name):
-    print(f"Calling a function, your name is {name}!")
+# def say_hello(name):
+#     print(f"Calling a function, your name is {name}!")
 
-def sum(a, b):
-    return a + b
+# def sum(a, b):
+#     return a + b
 
-name = "Rogerio"
-active = True
+# name = "Rogerio"
+# active = True
 
-print(f"Hello, {name}! Active status is {active}.");    #f before " formats the string with variables
-sayHello(name) # Call the function
+# print(f"Hello, {name}! Active status is {active}.");    #f before " formats the string with variables
+# say_hello(name) # Call the function
 
 
-total = sum(5, 10)  # Call the function that returns a value
-print(f"The total sum is: {total}")
+# total = sum(5, 10)  # Call the function that returns a value
+# print(f"The total sum is: {total}")
