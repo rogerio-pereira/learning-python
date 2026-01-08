0cb2ca832ec9db0cf80c7fb17498f179d744dc28
Rogerio Pereira <hi@rogeriopereira.dev>
2026-01-08 13:30:05 -0500

    chore(app): Clean all old app files (ready to start with masonite)

--- app/main.py
+++
@@ -1,5 +0,0 @@
-from util.license_plate import normalize_plate
-
-plate = input("License Plate? ")
-normalized_plate = normalize_plate(plate)
-print(f"Normalized Plate: {normalized_plate}")
--- app/util/__init__.py
+++
@@ -1,2 +0,0 @@
-# This file is intentionally left blank to mark the directory as a package.
-# It is required for Python to recognize 'util' as a module.
